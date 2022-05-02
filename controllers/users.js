47c98c26d391f4d3f9120ae8861d517b14af5988
p1d3c <mrkomnot@yandex.ru>
2022-05-02 19:39:40 +0300

    fix validation error for about

--- controllers/users.js
+++ controllers/users.js
@@ -47,11 +47,11 @@ module.exports.createUser = async (req, res) => {
 module.exports.updateUserProfile = async (req, res) => {
   try {
     const { name, about } = req.body;
-    if ((name.length < 2 || name.length > 30)
-    || (about.length < 2 || about.length > 30)) {
-      res.status(400).send({ message: 'Ошибка валидации данных' });
-      return;
-    }
+    // if ((name.length < 2 || name.length > 30)
+    // || (about.length < 2 || about.length > 30)) {
+    //   res.status(400).send({ message: 'Ошибка валидации данных' });
+    //   return;
+    // }
     await User.findByIdAndUpdate(
       req.user._id,
       { name, about },
@@ -59,16 +59,19 @@ module.exports.updateUserProfile = async (req, res) => {
     const updatedUser = await User.findById(req.user._id);
     res.status(200).send({ data: updatedUser });
   } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
+    }
     if (err.name === 'CastError' && err.path === '_id') {
       res.status(404).send({ message: 'Пользователь не найден' });
       return;
     }
     if (err.name === 'CastError' && err.path === 'about') {
-      res.status(400).send({ message: 'Переданы некоректные данные' });
+      res.status(400).send({ message: 'Переданы некорректные данные' });
       return;
     }
     if (err.name === 'CastError' && err.path === 'name') {
-      res.status(400).send({ message: 'Переданы некоректные данные' });
+      res.status(400).send({ message: 'Переданы некорректные данные' });
       return;
     }
     res.status(500).send({ message: 'server error', err });
